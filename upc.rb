f1bf6b277f9d0be271be684f3dd0107ea5ba079f
tolves <tov.sharing@gmail.com>
2025-11-15 13:21:48 +0000

    Refactor upc_count to handle game_log data
    Updated upc_count function to process game_log.tsv and capture unique portals with additional data fields.

--- upc.rb
+++ upc.rb
@@ -2,7 +2,7 @@
 
 def upc_count()
 
-	ori_data = read_file("portal_history.tsv")
+	ori_data = read_file("game_log.tsv")
 	data = ori_data
 	data = data.split(/\n/)
 	data.shift
@@ -14,35 +14,43 @@ def upc_count()
 
 	data.map do |x|
 		i = x.split(/\t/)
-		if i[0] == "Captured"
-			captured.push([i[1].to_f,i[2].to_f])
+		if i[3] == "captured portal"
+			captured.push([i[0],i[1],i[2]])
 		end
-		all_upv.push([i[1].to_f,i[2].to_f])
+		if ['hacked friendly portal','created link','mod deployed','resonator deployed','hacked enemy portal','resonator upgraded'].index(i[3]) 
+			all_upv.push([i[0],i[1],i[2]])
+		end
 	end
 
-	upc = captured.uniq
-	upv = all_upv.uniq
+	upc = captured.uniq { |x| [x[1], x[2]] }
+	upv = all_upv.uniq { |x| [x[1], x[2]] }
 
+	printf "Unique Portals Captured: #{upc.length}"
+	printf "Unique Portals Visited: #{upv.length}"
+
+
+	# draw_upc = '['
 	draw = '['
 
 	google_upc_map = []
 	upc_map = upc.each_slice(2000)
 	upc_map.each_with_index do |data,i|
-		google_upc_map[i] = "latitude,longitude\r" 
+		google_upc_map[i] = "timestamp,latitude,longitude\r" 
 		data.each do |x|
-			draw << "{\"type\":\"marker\",\"latLng\":{\"lat\":#{x[0]},\"lng\":#{x[1]}},\"color\":\"#246bce\"},"
-			google_upc_map[i] << "#{x[0]}\,#{x[1]}\r"
+			draw << "{\"type\":\"marker\",\"latLng\":{\"lat\":#{x[1]},\"lng\":#{x[2]}},\"color\":\"#246bce\"},"
+			google_upc_map[i] << "#{x[0]}\,#{x[1]}\,#{x[2]}\r"
 		end
 		write_file("upc_map_#{i}.csv",google_upc_map[i])
 	end
 
-	upv_map = upv.each_slice(2000)
+	upv_map = upv.each_slice(50000)
+	#upv_map = upv  # put all upv in one file
 	google_upv_map = []
 	upv_map.each_with_index do |data, i|
-		google_upv_map[i] = "latitude,longitude\r"
+		google_upv_map[i] = "timestamp,latitude,longitude\r"
 		data.each do |x|
-			draw << "{\"type\":\"marker\",\"latLng\":{\"lat\":#{x[0]},\"lng\":#{x[1]}},\"color\":\"#1aff00\"},"
-			google_upv_map[i] << "#{x[0]}\,#{x[1]}\r"
+			draw << "{\"type\":\"marker\",\"latLng\":{\"lat\":#{x[1]},\"lng\":#{x[2]}},\"color\":\"#1aff00\"},"
+			google_upv_map[i] << "#{x[0]}\,#{x[1]}\,#{x[2]}\r"
 		end
 		write_file("upv_map_#{i}.csv",google_upv_map[i])
 	end
